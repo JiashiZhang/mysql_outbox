a62cb1cdd094b79ecfee0d50d714b083e92e0a00
JiashiZhang <jiashi@wustl.edu>
2024-11-19 19:39:22 -0800

    更改

---
+++ src/main/java/org/example/mysql_outbox/MysqlOutboxApplication.java
@@ -0,0 +1,13 @@
+package org.example.mysql_outbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlOutboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlOutboxApplication.class, args);
+    }
+
+}
